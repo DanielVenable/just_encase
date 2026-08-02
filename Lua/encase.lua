6271506cdf329989b1681eea30d1f5780957542c
Daniel Venable <daniel.venable@proton.me>
2026-08-01 18:08:11 -0700

    removes lines on end encase mode

--- Lua/encase.lua
+++ Lua/encase.lua
@@ -6,21 +6,33 @@ Made for the competition "Just In Case".
 Note: I recommend you play the pack before trying to figure out what this does.
 ]]
 
+-- Constants:
 local ENCASE_KEY = "E"
 local ENCASE_MODE_UNDO = "toggle_encase_mode"
+local BORDER_OBJECT = "line"
+local INNER_OBJECT = "ice"
 
--- The unitids of any currently encased objects
-local encased_units = {}
+-- Mutables:
+local encased_unitids = {}
 local is_encase_mode_on = false
 
 table.insert(mod_hook_functions.keyboard_input, function(key)
     if editor.strings[MENU] == "ingame" and key[1] == ENCASE_KEY then
         if is_encase_mode_on then
             is_encase_mode_on = false
-            print("disable encase")
+
+            -- Deleting mutates the array, so we need to copy it first
+            local to_delete = {}
+            for k, v in ipairs(unitlists[BORDER_OBJECT]) do
+                to_delete[k] = v
+            end
+
+            for _, border in ipairs(to_delete) do
+                MF_specialremove(border, 2)
+                delete(border)
+            end
         else
             is_encase_mode_on = true
-            print("enable encase")
         end
         addundo({ ENCASE_MODE_UNDO, is_encase_mode_on })
         command("idle", 1)
@@ -33,6 +45,5 @@ table.insert(mod_hook_functions.level_start, function()
 end)
 
 undo_list[ENCASE_MODE_UNDO] = function(id, data)
-    print("undid past encase", data[2])
     is_encase_mode_on = not data[2]
 end
