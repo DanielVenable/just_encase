7af58f4e7d706886b4833cbe56e3077d7345eb66
Daniel Venable <daniel.venable@proton.me>
2026-08-03 15:14:31 -0700

    rearranged functions

--- Lua/encase.lua
+++ Lua/encase.lua
@@ -16,53 +16,7 @@ local INNER_OBJECT = "ice"
 local encased_ids = {}
 local is_encase_mode_on = false
 
-table.insert(mod_hook_functions.keyboard_input, function(key)
-    if editor.strings[MENU] == "ingame" and key[1] == ENCASE_KEY then
-        if is_encase_mode_on then
-            exit_encase_mode()
-        else
-            enter_encase_mode()
-        end
-        command("idle", 1)
-    end
-end)
-
-table.insert(mod_hook_functions.level_start, function()
-    is_encase_mode_on = false
-    encased_units = {}
-end)
-
-undo_list[ENCASE_MODE_UNDO] = function(_, data)
-    is_encase_mode_on = not data[2]
-    encased_ids = data[3]
-end
-
-table.insert(mod_hook_functions.block, function()
-    if is_encase_mode_on then
-        local encased_squares = find_all_encased_squares()
-        if #encased_squares > 0 then
-            exit_encase_mode()
-
-            for _, square in ipairs(encased_squares) do
-                local here = findallhere(square.x, square.y)
-                if #here > 0 then
-                    local _, id = create(INNER_OBJECT, square.x, square.y, 0)
-                    table.insert(encased_ids, id)
-                end
-                for _, obj in ipairs(here) do
-                    table.insert(encased_ids, mmf.newObject(obj).values[ID])
-                end
-            end
-        else
-            for _, you in ipairs(findallfeature(nil, "is", "you")) do
-                local unit = mmf.newObject(you)
-                create(BORDER_OBJECT, unit.values[XPOS], unit.values[YPOS], unit.values[DIR])
-            end
-        end
-    end
-end)
-
-function find_all_encased_squares()
+local function find_all_encased_squares()
     local function pair(x, y)
         return tostring(x) .. "," .. tostring(y)
     end
@@ -141,21 +95,7 @@ function find_all_encased_squares()
     return encased_squares
 end
 
-function enter_encase_mode()
-    is_encase_mode_on = true
-    delete_all(INNER_OBJECT)
-    addundo({ ENCASE_MODE_UNDO, true, encased_ids })
-    encased_ids = {}
-end
-
-function exit_encase_mode()
-    is_encase_mode_on = false
-    delete_all(BORDER_OBJECT)
-    addundo({ ENCASE_MODE_UNDO, false, encased_ids })
-    encased_ids = {}
-end
-
-function delete_all(obj_name)
+local function delete_all(obj_name)
      -- Deleting mutates the array, so we need to copy it first
     local to_delete = {}
     for k, v in ipairs(unitlists[obj_name] or {}) do
@@ -166,3 +106,63 @@ function delete_all(obj_name)
         delete(border)
     end
 end
+
+local function enter_encase_mode()
+    is_encase_mode_on = true
+    delete_all(INNER_OBJECT)
+    addundo({ ENCASE_MODE_UNDO, true, encased_ids })
+    encased_ids = {}
+end
+
+local function exit_encase_mode()
+    is_encase_mode_on = false
+    delete_all(BORDER_OBJECT)
+    addundo({ ENCASE_MODE_UNDO, false, encased_ids })
+    encased_ids = {}
+end
+
+table.insert(mod_hook_functions.keyboard_input, function(key)
+    if editor.strings[MENU] == "ingame" and key[1] == ENCASE_KEY then
+        if is_encase_mode_on then
+            exit_encase_mode()
+        else
+            enter_encase_mode()
+        end
+        command("idle", 1)
+    end
+end)
+
+table.insert(mod_hook_functions.level_start, function()
+    is_encase_mode_on = false
+    encased_units = {}
+end)
+
+undo_list[ENCASE_MODE_UNDO] = function(_, data)
+    is_encase_mode_on = not data[2]
+    encased_ids = data[3]
+end
+
+table.insert(mod_hook_functions.block, function()
+    if is_encase_mode_on then
+        local encased_squares = find_all_encased_squares()
+        if #encased_squares > 0 then
+            exit_encase_mode()
+
+            for _, square in ipairs(encased_squares) do
+                local here = findallhere(square.x, square.y)
+                if #here > 0 then
+                    local _, id = create(INNER_OBJECT, square.x, square.y, 0)
+                    table.insert(encased_ids, id)
+                end
+                for _, obj in ipairs(here) do
+                    table.insert(encased_ids, mmf.newObject(obj).values[ID])
+                end
+            end
+        else
+            for _, you in ipairs(findallfeature(nil, "is", "you")) do
+                local unit = mmf.newObject(you)
+                create(BORDER_OBJECT, unit.values[XPOS], unit.values[YPOS], unit.values[DIR])
+            end
+        end
+    end
+end)
